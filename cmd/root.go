464b9ffa323d2501f706d200ae8c9539512ef74f
melqtx <melqtx@proton.me>
2026-07-23 10:25:16 +0530

    repo cleanup: remove dead --config flag and viper plumbing from root, ignore xeet-bin

--- cmd/root.go
+++ cmd/root.go
@@ -2,16 +2,12 @@ package cmd
 
 import (
 	"fmt"
-	"os"
 
 	"xeet/internal/tui"
 	"xeet/pkg/config"
 
 	"github.com/spf13/cobra"
-	"github.com/spf13/viper"
 )
-
-var cfgFile string
 
 var (
 	appVersion   string
@@ -34,10 +30,7 @@ func SetVersion(version, commit, buildTime string) {
 }
 
 func init() {
-	cobra.OnInitialize(initConfig)
-	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.xeet.yaml)")
 	rootCmd.PersistentFlags().BoolP("verbose", "v", false, "verbose output")
-	_ = viper.BindPFlag("verbose", rootCmd.PersistentFlags().Lookup("verbose"))
 }
 
 func runTUI(cmd *cobra.Command, args []string) error {
@@ -50,20 +43,4 @@ func runTUI(cmd *cobra.Command, args []string) error {
 		}
 	}
 	return tui.Run()
-}
-
-func initConfig() {
-	if cfgFile != "" {
-		viper.SetConfigFile(cfgFile)
-	} else {
-		home, err := os.UserHomeDir()
-		cobra.CheckErr(err)
-		viper.AddConfigPath(home)
-		viper.SetConfigType("yaml")
-		viper.SetConfigName(".xeet")
-	}
-	viper.AutomaticEnv()
-	if err := viper.ReadInConfig(); err == nil && viper.GetBool("verbose") {
-		fmt.Fprintln(os.Stderr, "Using config file:", viper.ConfigFileUsed())
-	}
 }
